475359794e9a450b3a3db55e76762e84dc8b165a
Foniass <fonias.gaming@gmail.com>
2024-10-24 13:31:31 +0200

    try fix antibot on vps

--- lolstaticdata/common/utils.py
+++ lolstaticdata/common/utils.py
@@ -151,7 +151,21 @@ def download_soup(url: str, use_cache: bool = True, dir: str = f"__cache__"):
         with open(fn, encoding="utf-8") as f:
             html = f.read()
     else:
-        page = requests.get(url)
+        headers = {
+          "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+                       "(KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"),
+        "Accept": ("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,"
+                   "image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"),
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "https://www.google.com/",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "DNT": "1",  # Do Not Track Request Header
+        }
+
+        page = requests.get(url, headers=headers)
+        # page = requests.get(url)
         # html = page.content.decode(page.encoding)
         html = page.text
         if use_cache:
